defa48ef1b159c948733c52bd4e6a476676c6fd0
oleg-boyko <97238155+oleg-boyko@users.noreply.github.com>
2022-06-29 18:40:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import { goods } from './goods.js';
  * @param {string} brand
  * @returns {*}
  */
-const brandFilter = (brand) => {
-return goods.filter((goodBrand) => goodBrand.brand === brand); 
-};
+   const brandFilter = (brand) => {
+   return goods.filter((goodBrand) => goodBrand.brand === brand); 
+   };
 
 /**
  * @param {string} color
  * @returns {*}
  */
-const colorFilter = (color) => {
-return goods.filter((goodColor) => goodColor.color === color); 
-};
+   const colorFilter = (color) => {
+   return goods.filter((goodColor) => goodColor.color === color); 
+   };
 
 /**
  * @param {string} model
@@ -29,39 +29,39 @@ return goods.filter((goodModel) => goodModel.model === model);
  * @param {number} memory
  * @returns {*}
  */
-const memoryFilter = (memory) => {
-return goods.filter((goodMemory) => goodMemory.memory === memory); 
-};
+    const memoryFilter = (memory) => {
+    return goods.filter((goodMemory) => goodMemory.memory === memory); 
+    };
 
 /**
  * @param {number} price
  * @returns {*}
  */
-const priceFilter = (price) => {
-return goods.filter((goodPrice) => goodPrice.price === price); 
-};
+    const priceFilter = (price) => {
+    return goods.filter((goodPrice) => goodPrice.price === price); 
+    };
 
 /**
  * @param {string} country
  * @returns {*}
  */
-const countryFilter = (country) => {
-return goods.filter((goodCountry) => goodCountry.country === country); 
-};
+    const countryFilter = (country) => {
+    return goods.filter((goodCountry) => goodCountry.country === country); 
+    };
 
 /**
  * @param {string} os
  * @returns {*}
  */
-const osFilter = (os) => {
-return goods.filter((goodOs) => goodOs.os === os); 
-};
+   const osFilter = (os) => {
+   return goods.filter((goodOs) => goodOs.os === os); 
+   };
 
 /**
  * @param {number} from
  * @param {number} to
  */
-const rangeFilter = (from, to) => {
+      const rangeFilter = (from, to) => {
       return goods.filter((good) => (good.price >= from && good.price <= to));
       }; 
 
